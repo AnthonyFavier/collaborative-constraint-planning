6f7b2dac9a3b4eaaa18b061486810e49cbe85871
AnthonyFavier <anthony.favier@laas.fr>
2025-03-20 09:38:05 -0400

    comment initialized CM

--- cai.py
+++ cai.py
@@ -284,21 +284,18 @@ def CAI(problem_name, planning_mode):
             D6 - plane2 must be located in city2 in the final state
                 (at-end (located plane2 city2))
     """
-    r = CM.createRaw("never use plane1")
-    d = CM.createDecomposed(r, "Person1, person2, person3, and person4 should never be in plane1.")
-    d.encoding = "(always (and (not (in person1 plane1)) (not (in person2 plane1)) (not (in person3 plane1)) (not (in person4 plane1))))"
-    d = CM.createDecomposed(r, "Plane1 should remain at its initial location (city1) throughout the plan.")
-    d.encoding = "(always (located plane1 city1))"
-    d = CM.createDecomposed(r, "The number of people onboard plane1 should always be zero.")
-    d.encoding = "(always (= (onboard plane1) 0))"
-    d = CM.createDecomposed(r, "The fuel level of plane1 should remain unchanged from its initial value.")
-    d.encoding = "(always (= (fuel plane1) 174))"
-    r = CM.createRaw("plane2 should be in city2 at the end")
-    d = CM.createDecomposed(r, "plane2 must be located in city2 in the final state")
-    d.encoding = "(at-end (located plane2 city2))"
-    CM.show()
-    
-    # exit()
+    # r = CM.createRaw("never use plane1")
+    # d = CM.createDecomposed(r, "Person1, person2, person3, and person4 should never be in plane1.")
+    # d.encoding = "(always (and (not (in person1 plane1)) (not (in person2 plane1)) (not (in person3 plane1)) (not (in person4 plane1))))"
+    # d = CM.createDecomposed(r, "Plane1 should remain at its initial location (city1) throughout the plan.")
+    # d.encoding = "(always (located plane1 city1))"
+    # d = CM.createDecomposed(r, "The number of people onboard plane1 should always be zero.")
+    # d.encoding = "(always (= (onboard plane1) 0))"
+    # d = CM.createDecomposed(r, "The fuel level of plane1 should remain unchanged from its initial value.")
+    # d.encoding = "(always (= (fuel plane1) 174))"
+    # r = CM.createRaw("plane2 should be in city2 at the end")
+    # d = CM.createDecomposed(r, "plane2 must be located in city2 in the final state")
+    # d.encoding = "(at-end (located plane2 city2))"
     
     while True:
         
