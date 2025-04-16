15d796e355356fb2e15dda7d360a209f71ccfb1b
Ngoc La <ntmla@mit.edu>
2025-04-16 18:22:59 -0400

    Modify original for HDDL

---
+++ CAI_hddl.py
@@ -0,0 +1,393 @@
+import sys
+import LLM
+import tools
+from NumericTCORE.bin.ntcore import main as ntcore
+import click
+from planner import planner
+from defs import *
+from updatePDSimPlan import main as updatePDSimPlan
+import Constraints
+from UserOption import UserOption
+        
+CM = Constraints.ConstraintManager()
+
+
+def askConstraintsToAdd():
+    nl_constraints = []
+    while True:
+        if not len(nl_constraints):
+            mprint(color.BOLD + "\nEnter your first constraint:\n> " + color.END, end="")
+        else:
+            mprint(color.BOLD + "\nPress Enter to validate or type another constraint:\n> " + color.END, end="")
+            
+        t = input()
+        if t=="":
+            break
+        else:
+            nl_constraints.append(t)
+            
+    return nl_constraints
+def addConstraints(nl_constraints):
+    new_r = []
+    mprint(color.BOLD + "\nDecomposing constraints" + color.END)
+    for nl_constraint in nl_constraints:
+        encodingOK = False
+        i=0
+        while not encodingOK:
+            
+            r = CM.createRaw(nl_constraint)
+            new_r.append(r)
+            
+            if i==0:
+                mprint(color.BOLD + "\nDecomposing...\n" + str(r) + color.END)
+                result = LLM.decompose(g_domain, g_problem, nl_constraint)
+            else:
+                mprint(color.BOLD + "\nRe-Decomposing...\n" + str(r) + color.END)
+                result = LLM.redecompose("Decompose again the constraint while considering the following: " + feedback)
+                
+            result = LLM.removeFormating(result)
+            
+            # for c in constraints:
+            for c in result.splitlines():
+                CM.createDecomposed(r, c)
+            
+            mprint(r.strChildren())
+            
+            mprint("* Check Terminal *")
+            
+            answer = input("\nPress Enter to validate the decomposition or type a comment to consider when decomposing again: ")
+            encodingOK = answer==''
+
+            if encodingOK:
+                LLM.clear_message_history()
+            else:
+                id = r.symbol[1]
+                new_r.remove(r)
+                deleteConstraints([r.symbol])
+                del r
+                Constraints.Constraint._ID = int(id)
+                feedback = answer
+                
+            i+=1
+            
+    # When all ok, encode the decomposed of all new r
+    
+    # Encoding
+    mprint(color.BOLD + "\nEncoding..." + color.END)
+    r_to_delete = []
+    for r in new_r:
+        mprint(color.BOLD + str(r) + color.END)
+        for c in r.children:
+            encodingOK = False
+            MAX_ENCODING_TRY = 5
+            i=0
+            while not encodingOK and i<MAX_ENCODING_TRY:
+                
+                # 1 # Encode the preferences
+                if i==0: # first time
+                    mprint(f"\tencoding {c.symbol}...")
+                    encodedPref = LLM.encodePrefs(g_domain, g_problem, c.nl_constraint)
+                else: # Re-encoding
+                    mprint(f"\tre-encoding {c.symbol} (try {i+1}/{MAX_ENCODING_TRY}) ...")
+                    encodedPref = LLM.reencodePrefs(feedback)
+                filteredEncoding = tools.filterEncoding(encodedPref)
+                filteredEncoding = tools.initialFixes(filteredEncoding)
+                # mprint(filteredEncoding)
+                    
+                # 2 # Update the problem and verify the encoding
+                    # If error, re-encode with feedback
+                updatedProblem = tools.updateProblem(g_problem, [filteredEncoding])
+                encodingOK, feedback = tools.verifyEncoding(updatedProblem, g_domain, filteredEncoding)
+                if encodingOK:
+                    c.encoding = filteredEncoding
+                    LLM.clear_message_history()
+                else:
+                    mprint("\t\tVerifier: Encoding not OK.")
+                    # mprint(feedback)
+                i+=1
+            
+            if not encodingOK:
+                mprint(f"Failure: Maximum attempts reached to encode {c.symbol} of {r.symbol}... {r.symbol} will be deleted")
+                r_to_delete.append(r.symbol)
+                break
+    
+    if len(r_to_delete):
+        deleteConstraints(r_to_delete)
+                
+            
+def deleteConstraintsAsk():
+    loop = True
+    while loop:
+        loop = False
+        constraint_symbols = input("Which constraints you want to delete? (type symbol, separated by commas or space, leave empty to cancel)\n> ")
+        if constraint_symbols=='':
+            print("Deletion aborted.")
+            return None
+        
+        constraint_symbols = " ".join(constraint_symbols.split(',')).upper()
+        constraint_symbols = constraint_symbols.split()
+        for c in constraint_symbols:
+            if not c in CM.constraints:
+                print("One or more constraints not recognized.\n")
+                loop = True
+                break
+    
+    print('\n' + ", ".join(constraint_symbols) + " will be deleted.")
+    while True:
+        answer = input("Confirm (y/n)? ")
+        if answer in ['y', 'n']:
+            if answer=='n':
+                print("Deletion aborted.")
+                return None
+            break
+        
+    deleteConstraints(constraint_symbols)
+def deleteConstraints(constraint_symbols):
+    # if R# remove raw constraint and all decomposed associated
+    # if D# remove only decompose from general list and from parent children
+    
+    # Deletion
+    for symbol in constraint_symbols:
+        if symbol not in CM.constraints:
+            # already deleted
+            continue
+    
+        if symbol in CM.raw_constraints:
+            constraint = CM.constraints[symbol]
+            CM.constraints.pop(constraint.symbol)
+            CM.raw_constraints.pop(constraint.symbol)
+            for child in constraint.children:
+                CM.constraints.pop(child.symbol)
+                CM.decomposed_constraints.pop(child.symbol)
+                del child
+            del constraint
+            
+        elif symbol in CM.decomposed_constraints:
+            constraint = CM.constraints[symbol]
+            # Warning if deleting only child of a constraint
+            if len(constraint.parent.children)==1:
+                print(f"Warning: deleting {constraint.symbol} will also delete {constraint.parent.symbol}.")
+                while True:
+                    answer = input("Continue (y/n)? ")
+                    if answer in ['y', 'n']:
+                        if answer=='n':
+                            print("Deletion aborted.")
+                            return None
+                        break
+                
+            CM.constraints.pop(constraint.symbol)
+            CM.decomposed_constraints.pop(constraint.symbol)
+            constraint.parent.children.remove(constraint)
+            
+            if len(constraint.parent.children)==0:
+                CM.constraints.pop(constraint.parent.symbol)
+                CM.raw_constraints.pop(constraint.parent.symbol)
+                del constraint.parent
+            
+            del constraint
+            
+def activateConstraints():
+    # Constraints are activated by default
+    # ask which constraints to activate
+    # R# will activate all children D#
+    # D# will activate the corresponding decomposed constraint
+    
+    loop = True
+    while loop:
+        loop = False
+        x = input("Which constraints you want to activate? (type symbol, separated by commas or space, leave empty to cancel)\n> ")
+        if x=='':
+            print("Activation aborted.")
+            return None
+        
+        x = " ".join(x.split(',')).upper()
+        x = x.split()
+        for c in x:
+            if not c in CM.constraints:
+                print("One or more constraints not recognized.\n")
+                loop = True
+                break
+    
+    # Activation
+    for symbol in x:
+        CM.constraints[symbol].activate()
+        
+def deactivateConstraints():
+    # ask which constraints to deactivate
+    # R# will deactivate all children D#
+    # D# will deactivate the corresponding decomposed constraint
+    
+    loop = True
+    while loop:
+        loop = False
+        x = input("Which constraints you want to deactivate? (type symbol, separated by commas or space, leave empty to cancel)\n> ")
+        if x=='':
+            print("Deactivation aborted.")
+            return None
+        
+        x = " ".join(x.split(',')).upper()
+        x = x.split()
+        for c in x:
+            if not c in CM.constraints:
+                print("One or more constraints not recognized.\n")
+                loop = True
+                break
+    
+    # Deactivation
+    for symbol in x:
+        CM.constraints[symbol].deactivate()
+
+def planWithConstraints():
+    # get activated constraints
+    # update problem with activated constraints
+    # compile problem
+    # Solve it
+    
+    # Get activated constraints
+    activated_encodings = []
+    for k,c in CM.decomposed_constraints.items():
+        if c.isActivated():
+            activated_encodings.append(c.encoding)
+        
+    if not len(activated_encodings):
+        mprint("\nNo active constraints: Planning without constraints")
+        problem_name = g_problem_name
+    
+    else:
+        problem_name = PlanFiles.COMPILED
+        
+        updatedProblem = tools.updateProblem(g_problem, activated_encodings)
+        
+        # Save updated problem in a file
+        with open(UPDATED_PROBLEM_PATH, "w") as f:
+            f.write(updatedProblem)
+        
+        # Compile the updated problem
+        mprint(color.BOLD + "\nCompiling..." + color.END)
+        ntcore(DOMAIN_PATH, UPDATED_PROBLEM_PATH, "tmp/", achiever_strategy=NtcoreStrategy.DELTA, verbose=False)
+        
+    # Plan
+    feedback, plan = planner(problem_name, plan_mode=g_planning_mode, hide_plan=True, timeout=g_timeout)
+    success = feedback=='success'
+    if success:
+        mprint("\nSuccessful planning")
+        return plan
+    else:
+        mprint("\nFailed to plan")
+        return "Failed to plan:\n" + str(feedback)
+def askChangePlanMode():
+    print(f"Current planning mode: {g_planning_mode}")
+    mprint(f"Select a planning mode:\n\t1 - {PlanMode.ANYTIME}\n\t2 - {PlanMode.ANYTIMEAUTO}\n\t3 - {PlanMode.DEFAULT}\n\t4 - {PlanMode.OPTIMAL}\n\t5 - {PlanMode.SATISFICING}")
+    c = input("> ")
+    if c!='':
+        # Check if correct
+        if c in ['1', '2', '3', '4', '5']:
+            if c=='1':
+                CAI.g_planning_mode=PlanMode.ANYTIME
+            if c=='2':
+                CAI.g_planning_mode=PlanMode.ANYTIMEAUTO
+            if c=='3':
+                CAI.g_planning_mode=PlanMode.DEFAULT
+            if c=='4':
+                CAI.g_planning_mode=PlanMode.OPTIMAL
+            if c=='5':
+                CAI.g_planning_mode=PlanMode.SATISFICING
+            
+            print(f"\nPlanning mode set to: {g_planning_mode}")
+        else:
+            print("Incorrect input")
+            print("Aborted\n")
+    else:
+        print("Aborted\n")
+        
+g_last_plan = None
+def CAI():
+    global g_last_plan
+    
+    while True:
+        
+        # Show current CM
+        CM.show()
+        
+        # Options
+        UO = UserOption()
+        UO.addOption("ADD", "Add constraints")
+        UO.addOption("DEL", "Delete constraints")
+        UO.addOption("ACT", "Activate constraints")
+        UO.addOption("DEA", "Deactivate constraints")
+        UO.addOption("PLAN", "Plan with constraints")
+        UO.addOption("CHANGEPLANMODE", "Change planning mode")
+        UO.addOption("UPDATESIM", "Update Sim")
+        user_input = UO.ask()
+        
+        if "ADD"==user_input:
+            constraints = askConstraintsToAdd()
+            addConstraints(constraints)
+        elif "DEL"==user_input:
+            deleteConstraintsAsk()
+        elif "ACT"==user_input:
+            activateConstraints()
+        elif "DEA"==user_input:
+            deactivateConstraints()
+        elif "PLAN"==user_input:
+            g_last_plan = planWithConstraints()
+        elif "CHANGEPLANMODE"==user_input:
+            askChangePlanMode()
+        elif "UPDATESIM"==user_input:
+            if g_last_plan!= None and g_last_plan.find("Failed to plan:")!=-1:
+                updatePDSimPlan(g_last_plan)
+            else:
+                mprint("No current valid plan")
+
+def init(problem_name, planning_mode, timeout):
+    global g_problem_name, g_domain, g_problem, g_planning_mode, g_timeout
+    global DOMAIN_PATH, PROBLEM_PATH 
+    
+    if not problem_name in PROBLEMS:
+        click.echo("Unknown problem.\n" + KNOWN_PROBLEMS_STR)
+        exit()
+    
+    g_problem_name = problem_name
+    DOMAIN_PATH, PROBLEM_PATH = PROBLEMS[g_problem_name]
+    g_planning_mode = planning_mode
+    g_timeout = float(timeout) if timeout!=None else None
+    timeout_str = f', TO={g_timeout}' if g_timeout!=None else ''
+    
+    # Show selected problem
+    mprint(f"Planning mode: {planning_mode}{timeout_str}\nProblem ({problem_name}):\n\t- {DOMAIN_PATH}\n\t- {PROBLEM_PATH}")
+    
+    # Try parsing the initial problem
+    try:
+        parsed = tools.parse_pddl3(DOMAIN_PATH, PROBLEM_PATH)
+    except Exception as e:
+        print("ERROR", e)
+        raise Exception(f"Unable to parse the initial problem.")
+
+    # Set extracted fluent names (used during verification)
+    tools.set_fluent_names([f.name for f in parsed.problem.fluents])
+    objects = {}
+    for o in parsed.problem.all_objects:
+        if o.type.name in objects:
+            objects[o.type.name].append(o.name)
+        else:
+            objects[o.type.name] = [o.name]
+    tools.set_objects(objects)
+    
+    # Open initial problem
+    with open(DOMAIN_PATH, "r") as f:
+        g_domain = f.read()
+    with open(PROBLEM_PATH, "r") as f:
+        g_problem = f.read()
+
+@click.command(help=f"{KNOWN_PROBLEMS_STR}")
+@click.argument('problem_name')
+@click.option('-a', '--anytime', 'planning_mode', flag_value=PlanMode.ANYTIME, default=True, help="Set the planning mode to 'Anytime' (default)")
+@click.option('-d', '--default', 'planning_mode', flag_value=PlanMode.DEFAULT, help="Set the planning mode to 'Default'")
+@click.option('-o', '--optimal', 'planning_mode', flag_value=PlanMode.OPTIMAL, help="Set the planning mode to 'Optimal'")
+@click.option('-s', '--satisficing', 'planning_mode', flag_value=PlanMode.SATISFICING, help="Set the planning mode to 'Satisficing'")
+@click.option('-t', '--timeout', 'timeout', default=10.0, help="Timeout for planning")
+def main(problem_name, planning_mode, timeout):
+    init(problem_name, planning_mode, timeout)
+    CAI()
+if __name__ == '__main__':
+    main()
