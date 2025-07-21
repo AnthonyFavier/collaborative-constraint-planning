1855ad11b6240945cc7ca53d0e0310fe42f34a81
AnthonyFavier <anthony.favier@laas.fr>
2025-07-21 17:20:01 -0400

    update run_expe

--- run_expe.py
+++ run_expe.py
@@ -4,9 +4,9 @@ import time
 from datetime import datetime
 
 # job function
-def job(problem, mode, time_budget, id, max_id):
+def job(problem, mode, time_budget, id):
     time.sleep(0.1)
-    job_name = f"Job:{id}/{max_id} " + "-".join([problem, mode, str(time_budget)])
+    job_name = f"Job:{id}/{len(job_list)} " + "-".join([problem, mode, str(time_budget)])
     t0 = time.time()
     
     time_formatted = '[' + datetime.now().strftime("%H:%M:%S") + ']'
@@ -24,21 +24,24 @@ def job(problem, mode, time_budget, id, max_id):
     return None
 
 # Create job list
-modes = ['original', 'randomc', 'h-translation']
-problems = ['Rover10_n_t']
-time_budgets = [60, 90, 120, 200, 300, 400]
-max_id = len(modes)*len(problems)*len(time_budgets)+1
-
 job_list = []
 id = 1
+
+modes = ['original', 'randomc', 'h-translation']
+problems = ['ZenoTravel13', 'Rover10_n_t']
+time_budgets = [60, 90, 120, 200, 300, 400]
 for problem in problems:
     for mode in modes:
         for tb in time_budgets:
-            job_list.append([problem, mode, tb, id, max_id])
+            job_list.append([problem, mode, tb, id])
             id+=1
-          
-# Init global constraints  
-for problem in problems:
+            
+# Init global constraints
+all_problems = set()
+for j in job_list:
+    if j[0] not in all_problems:
+        all_problems.union(set([j[0]]))
+for problem in all_problems:
     solving.init_global_constraints(problem)
 
 # Use a process pool with max 10 workers
