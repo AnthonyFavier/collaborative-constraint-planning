d19f98c324e12718aaa66c799c98f89ab6891b82
AnthonyFavier <anthony.favier@laas.fr>
2025-08-26 10:02:41 -0400

    show status menu

--- GUI_agentic.py
+++ GUI_agentic.py
@@ -1051,6 +1051,86 @@ class PlanFrame(customtkinter.CTkFrame):
         self.previous_textbox.configure(state="disabled")
         self.write_previous_lock.release()
 
+from typing import Union, Tuple, Optional
+from customtkinter.windows.widgets.font import CTkFont
+from customtkinter.windows.widgets.theme import ThemeManager
+class StatusWindow(customtkinter.CTkToplevel):
+    """
+    Dialog with extra window, message, entry widget, cancel and ok button.
+    For detailed information check out the documentation.
+    """
+
+    def __init__(self,
+                 fg_color: Optional[Union[str, Tuple[str, str]]] = None,
+                 text_color: Optional[Union[str, Tuple[str, str]]] = None,
+                 button_fg_color: Optional[Union[str, Tuple[str, str]]] = None,
+                 button_hover_color: Optional[Union[str, Tuple[str, str]]] = None,
+                 button_text_color: Optional[Union[str, Tuple[str, str]]] = None,
+                 entry_fg_color: Optional[Union[str, Tuple[str, str]]] = None,
+                 entry_border_color: Optional[Union[str, Tuple[str, str]]] = None,
+                 entry_text_color: Optional[Union[str, Tuple[str, str]]] = None,
+
+                 title: str = "CTkDialog",
+                 font: Optional[Union[tuple, CTkFont]] = None,
+                 text: str = "CTkDialog"):
+
+        super().__init__(fg_color=fg_color)
+
+        self._fg_color = ThemeManager.theme["CTkToplevel"]["fg_color"] if fg_color is None else self._check_color_type(fg_color)
+        self._text_color = ThemeManager.theme["CTkLabel"]["text_color"] if text_color is None else self._check_color_type(button_hover_color)
+        self._button_fg_color = ThemeManager.theme["CTkButton"]["fg_color"] if button_fg_color is None else self._check_color_type(button_fg_color)
+        self._button_hover_color = ThemeManager.theme["CTkButton"]["hover_color"] if button_hover_color is None else self._check_color_type(button_hover_color)
+        self._button_text_color = ThemeManager.theme["CTkButton"]["text_color"] if button_text_color is None else self._check_color_type(button_text_color)
+        self._entry_fg_color = ThemeManager.theme["CTkEntry"]["fg_color"] if entry_fg_color is None else self._check_color_type(entry_fg_color)
+        self._entry_border_color = ThemeManager.theme["CTkEntry"]["border_color"] if entry_border_color is None else self._check_color_type(entry_border_color)
+        self._entry_text_color = ThemeManager.theme["CTkEntry"]["text_color"] if entry_text_color is None else self._check_color_type(entry_text_color)
+
+        self._user_input: Union[str, None] = None
+        self._running: bool = False
+        self._title = title
+        self._text = text
+        self._font = font
+
+        self.title(self._title)
+        self.lift()  # lift window on top
+        self.attributes("-topmost", True)  # stay on top
+        self.protocol("WM_DELETE_WINDOW", self._on_closing)
+        self.after(10, self._create_widgets)  # create widgets with slight delay, to avoid white flickering of background
+        self.resizable(False, False)
+        self.grab_set()  # make other windows not clickable
+
+    def _create_widgets(self):
+        self.grid_columnconfigure((0, 1), weight=1)
+        self.rowconfigure(0, weight=1)
+
+        self._label = customtkinter.CTkLabel(master=self,
+                               width=300,
+                               wraplength=300,
+                               fg_color="transparent",
+                               text_color=self._text_color,
+                               text=self._text,
+                               font=self._font)
+        self._label.grid(row=0, column=0, columnspan=1, padx=20, pady=20, sticky="ew")
+
+        self._ok_button = customtkinter.CTkButton(master=self,
+                                    width=100,
+                                    border_width=0,
+                                    fg_color=self._button_fg_color,
+                                    hover_color=self._button_hover_color,
+                                    text_color=self._button_text_color,
+                                    text='Ok',
+                                    font=self._font,
+                                    command=self._ok_event)
+        self._ok_button.grid(row=1, column=0, columnspan=1, padx=(20, 10), pady=(0, 20), sticky="ew")
+
+    def _ok_event(self, event=None):
+        self.grab_release()
+        self.destroy()
+
+    def _on_closing(self):
+        self.grab_release()
+        self.destroy()
+
 class App(customtkinter.CTk):
     font = ("Arial", 20, "bold")
     
@@ -1136,6 +1216,10 @@ class App(customtkinter.CTk):
         weathermenu.add_command(label="Deactivate fake weather", command=agentic_constraint.deactivateFakeWeather)
         menubar.add_cascade(label="Weather", menu=weathermenu)
         
+        helpmenu = Menu(menubar, tearoff=0)
+        helpmenu.add_command(label="Show status", command=self.showStatus)
+        menubar.add_cascade(label="Help", menu=helpmenu)
+        
         self.config(menu=menubar)
         
         # TODO: Escape also kills child threads
@@ -1201,3 +1285,25 @@ class App(customtkinter.CTk):
         self.buttons_frame.enableButtons()
         self.plan_frame.enableButtons()
         self.display_frame.enableButtons()
+        
+    def showStatus(self):
+        text = """
+E2NL: {e2nl}
+E2NL Review: {e2nl_review}
+Fake Weather: {fake_weather}
+Show decomps: {show_decomps}
+Show encodings: {show_encodings}
+Plan mode: {plan_mode}
+Time budget: {time_budget}
+"""[1:-1]
+        text = text.format(
+            e2nl = CAI.WITH_E2NL,
+            e2nl_review = agentic_constraint.REVIEW_E2NL,
+            fake_weather = agentic_constraint.FAKE_WEATHER,
+            show_decomps = self.constraints_frame.show_decomps,
+            show_encodings = self.constraints_frame.show_encodings,
+            plan_mode = CAI.g_planning_mode,
+            time_budget = CAI.g_timeout,
+        )
+        StatusWindow(title='Status', text=text)
+                
