ef619943ea0ade6ebf80fbbd37a32027ef151184
AnthonyFavier <anthony.favier@laas.fr>
2025-09-16 15:09:17 -0400

    Ablation stats tests

--- analyzeAblation.py
+++ analyzeAblation.py
@@ -1,6 +1,16 @@
 from scipy.stats import shapiro, wilcoxon, mannwhitneyu
 import csv
 
+import numpy as np
+from scipy.stats import bartlett
+from scipy.stats import levene
+from scipy.stats import normaltest
+
+from scipy.stats import describe, ranksums, fisher_exact
+from scipy.stats import hypergeom
+from statsmodels.stats.proportion import proportions_ztest
+
+
 def extractCSV(filename):
     with open(filename, newline='') as csvfile:
         reader = csv.reader(csvfile)
@@ -31,31 +41,22 @@ data = {
     'DECOMP_CONFIRM': extractCSV('/home/afavier/ws/CAI/export results for ablation/5-DECOMP_CONFIRM/DECOMP_CONFIRM.csv'),
 }
 
-
-
-import numpy as np
-from scipy.stats import bartlett
-from scipy.stats import levene
-from scipy.stats import normaltest
-
-
-
 # Homogeneity
 def testHomogeneity(set1, set2, var):
     print('')
 
-    ## Bartlett's Test
-    print(f"Bartlett: {set1}-{set2} {var}")
-    r = bartlett(data[set1][var], data[set2][var])
-    if r[1] < 0.05:
-        print('Reject the null hypothesis of equal variance between groups. (Unequal Variance)')
-        print(f'P-value is {r[1]}.')
-    else:
-        print('Fail to reject the null hypothesis of equal variance between groups. (suggest Equal Variance)')
-        print(f'P-value is {r[1]}.')
+    # ## Bartlett's Test
+    # print(f"Homogeneity (Bartlett): {set1}-{set2} {var}")
+    # r = bartlett(data[set1][var], data[set2][var])
+    # if r[1] < 0.05:
+    #     print('Reject the null hypothesis of equal variance between groups. (Unequal Variance)')
+    #     print(f'P-value is {r[1]}.')
+    # else:
+    #     print('Fail to reject the null hypothesis of equal variance between groups. (suggest Equal Variance)')
+    #     print(f'P-value is {r[1]}.')
 
     ## Levene's Test
-    print(f"Levene: {set1}-{set2} {var}")
+    print(f"Homogeneity (Levene): {set1}-{set2} {var}")
     r = levene(data[set1][var], data[set2][var])
     if r[1] < 0.05:
         print('Reject the null hypothesis of equal variance between groups. (Unequal Variance)')
@@ -69,7 +70,6 @@ testHomogeneity('DIRECT', 'VERIFIER', 'correctness')
 testHomogeneity('DECOMP', 'DECOMP_CONFIRM', 'correctness')
 
 # Normality
-
 def testNormality(set, var):
     print('')
     print(f"Normal: {set} {var}")
@@ -86,11 +86,9 @@ testNormality('VERIFIER', 'parsable')
 testNormality('DECOMP', 'correctness')
 testNormality('DECOMP_CONFIRM', 'correctness')
 
-from scipy.stats import describe, ranksums, fisher_exact
-from scipy.stats import hypergeom
-from statsmodels.stats.proportion import proportions_ztest
 
-print('  ')
+print('============================================')
+print('============================================')
 print(describe(data['DIRECT']['correctness']))
 print(describe(data['DECOMP_CONFIRM']['correctness']))
 testHomogeneity('DIRECT', 'DECOMP_CONFIRM', 'correctness')
@@ -111,6 +109,10 @@ N = table[:, 0].sum()
 start, end = hypergeom.support(M, n, N)
 hypergeom.pmf(np.arange(start, end+1), M, n, N)
 
+print('============================================')
+print('============================================')
+print('TESTS')
+
 res = fisher_exact(table, alternative='two-sided')
 print('two-sided')
 print(res)
@@ -125,3 +127,174 @@ print(res)
 # count = [27, 20]
 # nobs = [30, 30]
 # print(proportions_ztest(count, nobs, alternative='larger'))
+
+
+print('============================================')
+print('============================================')
+
+
+testNormality('DIRECT', 'correctness')
+testNormality('VERIFIER', 'correctness')
+testNormality('DECOMP', 'correctness')
+testNormality('DECOMP_CONFIRM', 'correctness')
+
+
+testHomogeneity('DIRECT', 'VERIFIER', 'correctness')
+testHomogeneity('DIRECT', 'DECOMP', 'correctness')
+testHomogeneity('DIRECT', 'DECOMP_CONFIRM', 'correctness')
+
+testHomogeneity('VERIFIER', 'DECOMP', 'correctness')
+testHomogeneity('VERIFIER', 'DECOMP_CONFIRM', 'correctness')
+
+testHomogeneity('DECOMP', 'DECOMP_CONFIRM', 'correctness')
+
+print('============================================')
+print('============================================')
+
+"""         success     failure
+DECOMP      20          10
+HUMAN       27          3
+"""
+
+print('============================================')
+print('============================================')
+
+ENCODING = 19
+VERIFIER = 20
+DECOMP = 20
+HUMAN = 27
+
+print('HUMAN-DECOMP')
+X1 = HUMAN
+X2 = DECOMP
+table = np.array([[X1, 30-X1],[X2, 30-X2]])
+M = table.sum()
+n = table[0].sum()
+N = table[:, 0].sum()
+start, end = hypergeom.support(M, n, N)
+hypergeom.pmf(np.arange(start, end+1), M, n, N)
+
+res = fisher_exact(table, alternative='two-sided')
+print('two-sided')
+print(res)
+res = fisher_exact(table, alternative='less')
+print('less')
+print(res)
+res = fisher_exact(table, alternative='greater')
+print('greater')
+print(res)
+
+print('============================================')
+print('============================================')
+
+print('HUMAN-VERIFIER')
+X1 = HUMAN
+X2 = VERIFIER
+table = np.array([[X1, 30-X1],[X2, 30-X2]])
+M = table.sum()
+n = table[0].sum()
+N = table[:, 0].sum()
+start, end = hypergeom.support(M, n, N)
+hypergeom.pmf(np.arange(start, end+1), M, n, N)
+
+res = fisher_exact(table, alternative='two-sided')
+print('two-sided')
+print(res)
+res = fisher_exact(table, alternative='less')
+print('less')
+print(res)
+res = fisher_exact(table, alternative='greater')
+print('greater')
+print(res)
+
+print('============================================')
+print('============================================')
+
+print('HUMAN-ENCODING')
+X1 = HUMAN
+X2 = ENCODING
+table = np.array([[X1, 30-X1],[X2, 30-X2]])
+M = table.sum()
+n = table[0].sum()
+N = table[:, 0].sum()
+start, end = hypergeom.support(M, n, N)
+hypergeom.pmf(np.arange(start, end+1), M, n, N)
+
+res = fisher_exact(table, alternative='two-sided')
+print('two-sided')
+print(res)
+res = fisher_exact(table, alternative='less')
+print('less')
+print(res)
+res = fisher_exact(table, alternative='greater')
+print('greater')
+print(res)
+
+print('============================================')
+print('============================================')
+
+print('DECOMP-ENCODING')
+X1 = DECOMP
+X2 = ENCODING
+table = np.array([[X1, 30-X1],[X2, 30-X2]])
+M = table.sum()
+n = table[0].sum()
+N = table[:, 0].sum()
+start, end = hypergeom.support(M, n, N)
+hypergeom.pmf(np.arange(start, end+1), M, n, N)
+
+res = fisher_exact(table, alternative='two-sided')
+print('two-sided')
+print(res)
+res = fisher_exact(table, alternative='less')
+print('less')
+print(res)
+res = fisher_exact(table, alternative='greater')
+print('greater')
+print(res)
+
+print('============================================')
+print('============================================')
+
+print('DECOMP-VERIFIER')
+X1 = DECOMP
+X2 = VERIFIER
+table = np.array([[X1, 30-X1],[X2, 30-X2]])
+M = table.sum()
+n = table[0].sum()
+N = table[:, 0].sum()
+start, end = hypergeom.support(M, n, N)
+hypergeom.pmf(np.arange(start, end+1), M, n, N)
+
+res = fisher_exact(table, alternative='two-sided')
+print('two-sided')
+print(res)
+res = fisher_exact(table, alternative='less')
+print('less')
+print(res)
+res = fisher_exact(table, alternative='greater')
+print('greater')
+print(res)
+
+print('============================================')
+print('============================================')
+
+print('VERIFIER-ENCODING')
+X1 = VERIFIER
+X2 = ENCODING
+table = np.array([[X1, 30-X1],[X2, 30-X2]])
+M = table.sum()
+n = table[0].sum()
+N = table[:, 0].sum()
+start, end = hypergeom.support(M, n, N)
+hypergeom.pmf(np.arange(start, end+1), M, n, N)
+
+res = fisher_exact(table, alternative='two-sided')
+print('two-sided')
+print(res)
+res = fisher_exact(table, alternative='less')
+print('less')
+print(res)
+res = fisher_exact(table, alternative='greater')
+print('greater')
+print(res)
