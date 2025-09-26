dd3040afddd0bd5c8f5231c398fcc5fdec58d890
AnthonyFavier <anthony.favier@laas.fr>
2025-09-26 12:19:25 -0400

    multi-line boxprint + tab fix

--- MILP/boxprint.py
+++ MILP/boxprint.py
@@ -27,17 +27,54 @@ ascii = {
         },
 }
 
-def boxprint(text, mode='s'):
+def boxprint(text: str, mode='s', tab_spaces=4, show=True):
+    # Set mode
     if mode=='s':
         mode = 'simple'
     if mode=='d':
         mode = 'double'
-        
-    length = len(text)
     
+    # Pre-changes
+    text = text.replace('\t', ' '*tab_spaces)
+    
+    # Splitting 
+    lines = text.splitlines()
+    
+    # Find max length/wide
+    length = -1
+    for l in lines:
+        if len(l)>length:
+            length = len(l)
+
+    # Top 
     top = ascii[mode]['top_left_corner'] + ascii[mode]['horizontal']*(length+2) + ascii[mode]['top_right_corner']
-    middle = ascii[mode]['vertical'] + ' ' + text + ' ' + ascii[mode]['vertical']
+    
+    # Middle text
+    middles = []
+    for l in lines:
+        filling_spaces = ' '*( length-len(l) )
+        middles.append( ascii[mode]['vertical'] + ' ' + l + filling_spaces + ' ' + ascii[mode]['vertical'] )
+    
+    # Bottom
     bottom = ascii[mode]['bot_left_corner'] + ascii[mode]['horizontal']*(length+2) + ascii[mode]['bot_right_corner']
     
-    print('\n'.join([top, middle, bottom]))
+    # Output text
+    output_text = '\n'.join([top, *middles, bottom])
+    
+    # Print
+    if show:
+        print(output_text)
+    
+    return output_text
 
+
+if __name__=='__main__':
+    print('test simple:')
+    boxprint("Hello")
+    
+    print('test double:')
+    boxprint("Hello", mode='d')
+    
+    print('test multi-line:')
+    boxprint("Hello\nHow are you?\nVery good thanks .....       e")
+    
