223b55be2f1a6d51e6358c31df4a364befa0af22
AnthonyFavier <anthony.favier@laas.fr>
2025-09-26 12:22:45 -0400

    scripts export env_var solvers

---
+++ MILP/export_cplex.sh
@@ -0,0 +1,6 @@
+export CPLEX_HOME="/opt/ibm/ILOG/CPLEX_Studio2211/cplex"
+export CPO_HOME="/opt/ibm/ILOG/CPLEX_Studio2211/cpoptimizer"
+export PATH="${PATH}:${CPLEX_HOME}/bin/x86-64_linux:${CPO_HOME}/bin/x86-64_linux"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${CPLEX_HOME}/bin/x86-64_linux:${CPO_HOME}/bin/x86-64_linux"
+export PYTHONPATH="${PYTHONPATH}:${CPLEX_HOME}/python/3.10/x86-64_linux"
+
