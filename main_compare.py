789638b284a86ef3ad85016d073ec82d90c57c39
AnthonyFavier <antfav24@mit.edu>
2025-10-17 10:59:03 -0400

    updated main compare

--- main_compare.py
+++ main_compare.py
@@ -1,12 +1,14 @@
 from run_compare_milp import run_compare as run_milp
 from run_compare_up import run_compare as run_up
 from MILP.start_with_timer import start_with_timer
-
+import time
 
 if __name__=='__main__':
 
     domains = ['counters', 'zenotravel', 'rover']
-
+    timeout = 300
     for d in domains:
-        start_with_timer(300, run_milp, d, i_start=1, i_last=20)
-        start_with_timer(300, run_up, d, i_start=1, i_last=20)
+        start_with_timer(timeout, run_milp, d, i_start=1, i_last=20)
+        time.sleep(1)
+        start_with_timer(timeout, run_up, d, i_start=1, i_last=20)
+        time.sleep(1)
