135398789781cb64b63eb9f606211379fa84a8d3
Nicole <ntmla@mit.edu>
2025-10-31 12:12:51 -0400

    add Nicole's path

--- updatePDSimPlan.py
+++ updatePDSimPlan.py
@@ -5,7 +5,8 @@
 # PDSIM_INSTANCE_PATH = "/home/afavier/pdsim/zenotravel_pdsim/Assets/Scenes/Zeno13/Data/PdSimInstance.asset"
 # PDSIM_INSTANCE_PATH = "/home/afavier/my_pdsim/PDSim/Assets/Scenes/Zeno13/Data/PdSimInstance.asset"
 # PDSIM_INSTANCE_PATH = '/home/afavier/pdsim/zenotravel_pdsim/Assets/Scenes/Zeno13/Data/PdSimInstance.asset'
-PDSIM_INSTANCE_PATH = '/home/afavier/pdsim/zenotravel_pdsim/Assets/Scenes/ZenoR/Data/PdSimInstance.asset'
+# PDSIM_INSTANCE_PATH = '/home/afavier/pdsim/zenotravel_pdsim/Assets/Scenes/ZenoR/Data/PdSimInstance.asset'
+PDSIM_INSTANCE_PATH = '/home/nicole/research/PDSim_Scenes_zenotravel/Assets/Scenes/ZenoR/Data/PdSimInstance.asset'
 # PDSIM_INSTANCE_PATH = "/home/afavier/my_pdsim/PDSim/Assets/Scenes/Rover3/Data/PdSimInstance.asset"
 
 def createActionStr(name, *parameters):
